fce7c2041ab1da6c0269425180161bb2f230159f
Asya Pritsker <aipritsker@gmail.com>
2013-10-24 16:49:14 -0400

    Update hive_winter.m

--- hive_winter.m
+++ hive_winter.m
@@ -5,35 +5,18 @@ function [yres, yV, yP, yH, yR] = hive_winter(year,aw,a,s,y,resIn,Vin,Pin,Hin,Ri
     summerdays = s;
     yeardays = y;
     T = year;
-    res = resIn;
-    P = Pin;
-    V = Vin;
-    H = Hin;
-    R = Rin;
 
-	N = zeros(agemaxwinter,1);
+	P0 = Pin(1,summerdays);
 
-	N(1:3) = res(1,summerdays)/3;
+    V0 = Vin(1,summerdays); 
 
-	N(4:11) = res(2,summerdays)/8;
+    H0 = Hin(1,summerdays);
 
-	N(12:26) = res(3,summerdays)/15;
-
-    N(27:agemax) = (res(4,summerdays)+res(5,summerdays)+res(6,summerdays))/34;
-    % this doesn't make sense- it's like artificially aging many of them.  
-	%N(27:agemaxwinter)=(res(4,summerdays)+res(5,summerdays)+res(6,summerdays))/124; 
-
-	P0 = P(1,summerdays);
-
-    V0 = V(1,summerdays); 
-
-    H0 = H(1,summerdays);
-
-    R0 = R(1,summerdays);
+    R0 = Rin(1,summerdays);
     
 
-    Y = [ V0; P0; H0; R0; N ];
-
+    Y = [ V0; P0; H0; R0; resIn ];
+    disp(Y(1:4))
 % 	clear res V P H R;
 
 	res=zeros(6,yeardays-summerdays);
@@ -48,33 +31,34 @@ function [yres, yV, yP, yH, yR] = hive_winter(year,aw,a,s,y,resIn,Vin,Pin,Hin,Ri
     	
 
     for t = (yeardays*T+summerdays+1):(yeardays*(T+1))
-        
+         
         Y = one_winter_day(Y,t);
-        
+        disp(Y(1:4))
         %wintpop = W*Yw; %this is a problem for some reason
         res(1:3,(t-(yeardays*T+summerdays))) =  Y(1:3); %wintpop(1:3);
         res(5,(t-(yeardays*T+summerdays))) = Y(4); %wintpop(4);
+        disp(res(:,(t-(yeardays*T+summerdays))))
         
         V(1,(t-(yeardays*T+summerdays))) = Y(1);
-        if Y(1)== 0
-            disp('ran out of space, on day:')
-            disp(t)
-            break
-        end
         P(1,(t-(yeardays*T+summerdays))) = Y(2);
-        if Y(2) == 0
-            disp('Hive starved, no pollen, on day:')
-            disp(t)
-            break
-        end
         H(1,(t-(yeardays*T+summerdays))) = Y(3);
-        
-        if Y(3) == 0
-            disp('Hive staved, no honey, on day:')
-            disp(t)
-            break
-        end
         R(1,(t-(yeardays*T+summerdays))) = Y(4);
+        
+%        if Y(1)== 0
+%           disp('ran out of space, on day:')
+%           disp(t)
+%           break
+%        end
+%         if Y(2) == 0
+%             disp('Hive starved, no pollen, on day:')
+%             disp(t)
+%             break
+%         end
+%         if Y(3) == 0
+%             disp('Hive starved, no honey, on day:')
+%             disp(t)
+%         end
+
     end %END OF LOOP THROUGH WINTER
     
 	yres = res ;
