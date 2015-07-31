d7fc2fb423af55947532129bd72abd4eef4a3be6
treluga <treluga@psu.edu>
2015-07-31 17:58:30 -0400

    refactoring plot script

--- timfigs.m
+++ timfigs.m
@@ -1,27 +1,32 @@
-function timfigs(YMatrix1, YMatrix2,Y1)
+function timfigs(STAGE, STORAGE, EGGS)
 
+	L = length(STORAGE);
 	% Create figure
 	figure(1);
 	clf();
 	subplot(3,1,1);
-	plot(YMatrix1);
+	plot(STAGE);
 	legend('Egg','Larva','Pupa','Nurse Bee','House Bee','Forager');
 	ylabel('Number of Bees','fontsize',24);
 	title('A Honey Bee Colony Population Dynamics','fontsize',24);
+	xlim([0,L]);
 
 	subplot(3,1,2);
-	%plot(YMatrix2(:,1),'gx-', YMatrix2(:,2),'bx-');
+	%plot(STORAGE(:,1),'gx-', STORAGE(:,2),'bx-');
 	%legend('Pollen','Honey');
-	plot(YMatrix2(:,1),'g-','linewidth',3);
-	legend('Pollen');
+	plot(STORAGE(:,1),'g-','linewidth',3);
+	title('Pollen Stores','fontsize',24);
 	ylabel('Number of Cells','fontsize',24);
+	xlim([0,L]);
 
 	subplot(3,1,3)
-	weight = (.047*YMatrix1(:, 2)+.158*YMatrix1(:, 3) + .133*(YMatrix1(:, 4)+YMatrix1(:, 5)+YMatrix1(:, 6)) + .23*YMatrix2(:, 1)+ .5*YMatrix2(:, 2))/1000;
+	weight = (.047*STAGE(:, 2)+.158*STAGE(:, 3) + .133*(STAGE(:, 4)+STAGE(:, 5)+STAGE(:, 6)) + .23*STORAGE(:, 1)+ .5*STORAGE(:, 2))/1000;
 	plot(weight,'k-','linewidth',3);
 	% Create ylabel
-	ylabel('Weight (kg) - no equip','fontsize',24);
+	title('Hive weight - no equipment','fontsize',24);
+	ylabel('Weight (kg)','fontsize',24);
 	xlabel('Day','fontsize',24);
+	xlim([0,L]);
 
 	print -depsc timoutput.eps
 
