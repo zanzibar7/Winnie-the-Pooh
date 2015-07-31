b1f6af3485181af67afa2c6780f49026da0da397
treluga <treluga@psu.edu>
2015-07-31 10:54:14 -0400

    whitespace and code sorting of one_summer_day.m

--- one_summer_day.m
+++ one_summer_day.m
@@ -1,13 +1,10 @@
-function nextstate = one_summer_day(state,date,STAGEMATRIX)% bee model in the field season 
+function nextstate = one_summer_day(state, date, STAGEMATRIX)% bee model in the field season 
 
 global hsurfX hsurfY hsurf; % the interpolated surface of NectarODE and honeycollection 
 
-agemax = max(size(STAGEMATRIX)); % indexing in matlab starts at 1, so add an extra day
-n_brood = sum(sum(STAGEMATRIX')(1:3));
-
-p_precocious = 0; % probability of individual nurse bee precociously developing to forager
-p_reversion = 0; % reversed prob. between foragers and house bees;
-FactorBroodNurse = 2.65 ; % One nurse bee can heat 2.65 brood cells - NOT CRUCIAL.. but probably closer to 5
+p_precocious = 0; % probability of nurse bee precociously developing to forager
+p_reversion = 0; % reversed probability between foragers and nurse bees;
+FactorBroodNurse = 2.65; % One nurse bee can heat 2.65 brood cells - NOT CRUCIAL.. but probably closer to 5
 p_slow = 0*1e-10; %probability of individual bee retardant developing to next age class
 
 queen_efficiency = 1; % downregulation of queen egg laying, used in perturbed hive scenarios
@@ -31,18 +28,20 @@ honeyconsumption = [ 0, 0.0297, 0, 0, 0.05 , 0.05 ];
 %higher consumption means higher need, and thus more brought in.
 pollenconsumption = [0, 0.0047, 0, 0.028, 0, 0 ];
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% %% Current conditions in bee hive %%%%%%%%
-Vt = state(1); % vacant cells 
-Pt = state(2); % pollen stores
-Ht = state(3); % honey stores at time t. 
-% We don't care about state(4), because those are now the 1-day old eggs,
-% and the new state(4) will only depend on how many eggs are layed now.
-Nt = state(5:end);% bee number at time t 
-stage = STAGEMATRIX*Nt;
+% (Blaschon et al.,1999) The modeled colony regulates the pollen stores around
+% a level that represents a reserve for approximately 6 days, based on the
+% current level of demand.
+Factorstore=6;
+
+% 0.48 is the pollen collected each day each forager, based on the amount
+% of pollen collected per foraging trip(0.06 cellful pollen,Camazine et
+% al., 1990), the average foraging trips performed per forager per day(10 trips per day) and the stochastic factor for each pollen
+% forager to make a successful foraging trip(80%)
+foragingsuccess = 0.48;
+
+relativedate = mod(date,360);  %% BUG!!! hard-coded dependence on year length
 
 %% Queen reproduction potential (McLellan et al., 1978)
-relativedate = mod(date,360);  %% BUG!!! hard-coded dependence on year length
 maxProduction = (0.0000434)*(relativedate)^4.98293*exp(-0.05287*relativedate);
 
 %% SURVIVORSHIP PARAMETERS
@@ -67,7 +66,22 @@ maxProduction = (0.0000434)*(relativedate)^4.98293*exp(-0.05287*relativedate);
 % st5 = 0.85; % 0.96-88.6%--survivorship for house bee stage
 % st6 = 0.78; % 78.5%--survivorship for forager bee stage
 
-stageship = [0.85, 0.85, 0.86, 0.85, 0.85, 0.78];
+stageship = [0.85, 0.85, 0.86, 0.85, 0.85, 0.78]; % baseline state survivorships
+
+agemax = max(size(STAGEMATRIX)); % indexing in matlab starts at 1, so add an extra day
+n_brood = sum(sum(STAGEMATRIX')(1:3));
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %% Current conditions in bee hive %%%%%%%%
+Vt = state(1); % vacant cells 
+Pt = state(2); % pollen stores
+Ht = state(3); % honey stores at time t. 
+% We don't care about state(4), because those are now the 1-day old eggs,
+% and the new state(4) will only depend on how many eggs are layed now.
+Nt = state(5:end);% bee number at time t 
+
+stage = STAGEMATRIX*Nt;
 
 %  %%%%%%%%%%% Fungicide treatments
 %  %day of the year on which pesticide treatment was applied : 
@@ -86,15 +100,6 @@ stageship = [0.85, 0.85, 0.86, 0.85, 0.85, 0.78];
 
 
 
-%% Index for the quality of pollen status and nursing quality in the colony
-% Negative feedback loops of pollen stores and nursing quality in affecting
-% the bee dynamics (Larva, Nurse) in turn affecting food collection and
-% storage.
-
-% (Blaschon et al.,1999) The modeled colony regulates the pollen stores around
-% a level that represents a reserve for approximately 6 days, based on the current level of demand.
-Factorstore=6;
-
 % The colony pollen demand includes the need of egg, larval, nurse and house bee stage. 
 % We assume the daily demand of pollen of bees is constant stage-specific parameters.
 PollenDemand = pollenconsumption*stage;
@@ -104,48 +109,52 @@ if ( (PollenDemand <= 0) || (HoneyDemand <= 0) )
     disp('PollenDemand or HoneyDemand leq zero, dead hive')
 end
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Bee Dynamics : Everyone ages by one day
+
+% Index for the quality of pollen status and nursing quality in the colony
+% Negative feedback loops of pollen stores and nursing quality in affecting
+% the bee dynamics (Larva, Nurse) in turn affecting food collection and
+% storage.
+
 % the level of the pollen stores in relation to the demand situation of the colony.
 %got rid of +1s in the denominators
 Indexpollen = max([0  min([1 Pt/(PollenDemand*Factorstore + 1) ])] );%max(0,min(1,Pt/(PollenDemand*Factorstore)))
-Indexhoney = max([0 min([1 Ht/HoneyDemand])]); %max(0,min(1,Ht/(HoneyDemand)))
-%the level of the active nurse bees population in relation to the total nursing demand of all brood stages.
+% The level of the active nurse bees population in relation to the total nursing demand of all brood stages.
 IndexNursing = max(0,min(1,stage(4)/((stage(2)+stage(1))*FactorBroodNurse+1)));
+% Indexhoney = max([0 min([1 Ht/HoneyDemand])]); %max(0,min(1,Ht/(HoneyDemand)))
 
 
-%% Bee Dynamics : Everyone ages by one day
-
-% determine nonlinear survivorships
+% nonlinear feedbacks on survivorships
 stageship = stageship.*[1, min(1,max(0,1-0.15*(1-Indexpollen*IndexNursing))), \
 	1, max(0,min(1,1-IndexNursing)), 1, 1];
 survivorship = (stageship.^(1./sum(STAGEMATRIX')))*STAGEMATRIX;
-
-% survivorship(1:3) = st1^(1/3); % the daily survival rate of egg stage at age(i=1-3) 
-% survivorship(4:11) = (st2*min(1,max(0,1-0.15*(1-Indexpollen*IndexNursing))))^(1/8); %  LARVA 
-% % st2: the time independent base mortality rate of larval stage at any age (4-11 days old- total 8 days)
-% % Larvae are frequently cannibalized in a honeybee colony.
-% % The rate of cannibalism depends on the age of the larvae (Schmickl and Crailsheim, 2001),
-% % the pollen status of the colony (Schmickl and Crailsheim, 2001)and the nursing quality (Eischen et al., 1982).
-% % Therefore, larval mortality includes a time-independent base mortality
-% % rate and the cannibalism factor. 0.15--the time-independent base
-% % cannibalism mortality rate for larval stage. 
-% survivorship(12:26)= st3^(1/15);
-% % PUPA: st3 is overal stage survival, cummulative over 15 days
+%
+% survivorship(1:3) is the daily survival rate of egg stage at age(i=1-3) 
+%
+% survivorship(4:11) is the time independent base mortality rate of larval
+% stage at any age (4-11 days old- total 8 days) Larvae are frequently
+% cannibalized in a honeybee colony.  The rate of cannibalism depends on the
+% age of the larvae (Schmickl and Crailsheim, 2001), the pollen status of the
+% colony (Schmickl and Crailsheim, 2001)and the nursing quality (Eischen et
+% al., 1982).  Therefore, larval mortality includes a time-independent base
+% mortality rate and the cannibalism factor. 0.15--the time-independent base
+% cannibalism mortality rate for larval stage. 
+%
+% survivorship(12:26) is pupae stage survival, cummulative over 15 days
 % 
-% % NURSE: who don't precociously forage (1-u) and who survive one day of 16 that make up st4
-% %It will be varied by the nursing efforts. A higher nursing load will
-% %cause a higher mortality of the nurse bee stage.
-% survivorship(27:42)= (1-u)*(st4*max(0,min(1,1-IndexNursing)))^(1/16) ; %= (1-u)*st4^(1/16);
-% % = (1-u)*(st4*max(0,min(1,1-IndexNursing)))^(1/16) ;
-% % = (1-u)*max(0,(1-(1-st4)*IndexNurseload))^(1/16);
+% survivorship(27:42) is nurse survivorship NURSE: who don't precociously
+% forage and who survive one day of 16 that make up st4 It will be varied by
+% the nursing efforts. A higher nursing load will cause a higher mortality of
+% the nurse bee stage.
 % 
-% %survivorship of HOUSE bee
-% survivorship(43:48)= st5^(1/6);%(st5*min(1,1-Indexhoney))^(1/6);
+% survivorship(43:48) is survivorship of HOUSE bees
 % 
-% survivorship(49:agemax)= (1-v)*st6^(1/12); % v is reversed probability of the forager bee stage to revert back to in-hive nurse bees. 
+% survivorship(49:agemax) is survivorship of forager bees
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Everything here just relates to storage and matrix multiplication.
+% Everything here just relates to matrix multiplication.
 A = diag(survivorship);
 theta = p_slow*ones(agemax,1); % probabilities of retarded development
 theta(1) = 0.; % can not have retardation of first stage
@@ -172,18 +181,18 @@ end
 
 %% Food is consumed, new eggs are layed
 
-% pollen consumption of egg, larval, nurse and house bee stage
-foodeaten =  min([Pt, PollenDemand]); 
-
 % The removal of dead brood, hygenic behavior gives total number of scavanged cells
 % -- this was the line that was generating an error before- causing Vt to be a matrix
 scavanged = (1-survivorship(1:n_brood))*Nt(1:n_brood);
+
+% pollen consumption of egg, larval, nurse and house bee stage
+polleneaten =  min([Pt, PollenDemand]); 
 
 % honey consumption of larval, nurse, house and forager bee stage
 honeyeaten= min([Ht, HoneyDemand]); 
 
 % Empty Cells due to the cleaned food cells and adult emergence
-vacated = Nt(n_brood) + foodeaten + honeyeaten; %check this, maybe not Nt(26)- maybe Nt1(26)?
+vacated = Nt(n_brood) + polleneaten + honeyeaten; %check this, maybe not Nt(26)- maybe Nt1(26)?
 Vt = Vt + vacated + scavanged ;
 %Actual eggs layed by queen this day determined here
 if (stage(4)+stage(5)+stage(6))<= 10 % the minimum requirement of number of bees needed to be around a queen bee
@@ -217,12 +226,6 @@ end
 % to current pollen storage.
 PollenNeed=max(0,PollenDemand*Factorstore-Pt);
 
-% 0.48 is the pollen collected each day each forager, based on the amount
-% of pollen collected per foraging trip(0.06 cellful pollen,Camazine et
-% al., 1990), the average foraging trips performed per forager per day(10 trips per day) and the stochastic factor for each pollen
-% forager to make a successful foraging trip(80%)
-foragingsuccess = 0.48;
-
 %Number of pollen foragers to recruit
 NeedPollenForager=PollenNeed/foragingsuccess; 
 
@@ -236,9 +239,9 @@ PollenForager=max(stage(6)*0.01, min(NeedPollenForager,stage(6)*.33)); %%THIS on
 
 % pollen storage depends on the available cells in the hive
 % and the foraging collection efficiency of the pollen forager---assumption for pollen foraging behavior
-storedfood = max([0, min([PollenForager*0.48, Vt])]);
+storedpollen = max([0, min([PollenForager*0.48, Vt])]);
 
-Vt = Vt - storedfood ;
+Vt = Vt - storedpollen;
 if Vt == 0
 	disp('ran out of space after food stored')
 	disp(date)
@@ -250,18 +253,19 @@ end
 % Nectar being processed into honey is reduced in volume by a factor .4
  
 predictedhoney=interp2(hsurfX,hsurfY,hsurf,stage(5),(stage(6)-PollenForager));
-if ( 0==exist('predictedhoney','var') || isnan(predictedhoney) || predictedhoney<0 )
+if ( 0==exist('predictedhoney','var') || isnan(predictedhoney) \
+		|| predictedhoney<0 )
 	predictedhoney=1.e-3;
 end
 storedhoney = min([predictedhoney, Vt]);
     
-Vt = Vt - storedhoney ;
+Vt = Vt - storedhoney;
  
 %% Pollen, Honey, Cells net input 
-Pt = max(0, Pt - foodeaten + storedfood); % Updated pollen stores at end of day
+Pt = max(0, Pt - polleneaten + storedpollen); % Updated pollen stores at end of day
 Ht = Ht + storedhoney - honeyeaten; % Updated honey stores at end of day, capped by total size of hive
 Nt = A*Nt; % structured dynamics for bees - output is a vector
-Nt(1) = R; %R; %number of eggs laid today, these are now the age zero eggs
+Nt(1) = R; % number of eggs laid today, these are now the age zero eggs
 
 nextstate = [Vt; Pt; Ht; R; Nt];
 
